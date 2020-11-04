e386f29005aa22890ca363b66cf490f5eea69ddf
Siva-SRC <62762339+Siva-SRC@users.noreply.github.com>
2020-11-04 16:06:55 +0530

    Update Love.js

--- src/Love.js
+++ src/Love.js
@@ -30,7 +30,7 @@ export default class Love extends React.Component {
             <div className="love">
                 <h1 className="love__heading">I love you di Jillu</h1>
                 <img 
-                src="https://lh3.googleusercontent.com/F96tur3je2TF4SiKMWPcCSw5v_324EMrq0brfu4tuM0uPY1ESzGdso7GPOP7DNqTmGqjmECcc8XVkciSCV5pWdRM6VauCGhiISDyNY4Xp5pouR77AgyzgBDtJ84nwolsoM4ZX0GoVTMpXsi0IvLXv-xXRz3O_yBoD3AYz5fWlizKEtZ0nh8LAjLUdE8dCTJV4CjTfIOkQGDuJZ2wmAHZQvHFMFgA3iruwYkjXUogeYerK8A0udS8rZhmEfoDWuzp3E98fkyTmgBh7Vfs4U1iyh3wXzEDYhziM2m0mr28ClDHnDQuUhA3coOeY_hXcgc4OZzYlBAM5TkVIn2pKRfDxn4cFpDj6tPCpioy3XZDWB2hiJOa6otNycPkOSyZPH-QCXR6USI7QZoi93ACNFEVFftJ41CfrQQWpxpTCe0zcagS4Bih3Dw78FMuMiwuudVESjjfT1pLwJTyDbtcCgLABCtI2nNoWOxWOYYEaS5FgasmNqGWXTN02VTVpCg_fi2OODDK8E840DuEfb2ibkAVPlEqbnz6xDGpByuJ50NiV6_Csfbj5G9YJlLcg5lAS4R_1WLAHUqhboEeuOA2YybWsZodsQYBJJqDCwErveka_i5dezWDrfchCEw0CjQIFmAdmB3s7ntcj_aCclpzuO3K4LE2NUR5w1T3QRO0560ba3reAMUugQjStv8W7rtvbg=w702-h936-no?authuser=1"
+                src="https://4kwallpapers.com/images/wallpapers/couple-sunset-proposal-silhouette-romantic-lovers-together-3840x2560-448.jpg"
                 alt=""
                 className="love__image"
                 />
