bbde096d1693618727b32794ca2cc026df295112
Siva-SRC <62762339+Siva-SRC@users.noreply.github.com>
2020-11-04 16:39:10 +0530

    Update Love.js

--- src/Love.js
+++ src/Love.js
@@ -8,7 +8,7 @@ export default class Love extends React.Component {
             play: false,
             pause: true
         }
-        this.url = "https://youtu.be/mqUYGNZ6kAY";
+        this.url = "https://quz1yp-a.akamaihd.net/downloads/ringtones/files/mp3/un-swaasam-tamilmp3song-net-19580.mp3";
         this.audio = new Audio(this.url);
     }
     play =() => {
